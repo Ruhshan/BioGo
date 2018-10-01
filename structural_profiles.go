3e8171b3d32c39f40f09defc9e177681d80c6220
Ruhshan <ruhshan.ahmed@gmail.com>
2018-10-01 21:21:24 +0600

    added gcskew function

---
+++ structural_profiles.go
@@ -0,0 +1,63 @@
+package biogo
+
+import (
+	"errors"
+	"fmt"
+)
+
+type gcskew struct {
+	ID     string
+	Values []string
+}
+
+func gcskewCalc(sequence string) string {
+	a, t, g, c := 1.0, 1.0, 1.0, 1.0
+
+	for _, s := range sequence {
+
+		switch s {
+		case 65:
+			a++
+		case 84:
+			t++
+		case 67:
+			c++
+		case 71:
+			g++
+
+		}
+	}
+
+	//return string((g - c) / (g + c))
+	return fmt.Sprintf("%.6f", (g-c)/(g+c))
+}
+
+func CalculateGCSkew(fastas []*fasta, window int, step int, max_len int) ([]*gcskew, error) {
+
+	gcskews := []*gcskew{}
+
+	for _, fasta := range fastas {
+
+		if len(fasta.Sequence) < max_len {
+			return gcskews, errors.New("Sequence Length is smaller than max length")
+		}
+
+		sequence := fasta.Sequence
+
+		new_gcskew := new(gcskew)
+
+		new_gcskew.ID = fasta.Id
+
+		for i := 0; i < max_len-window; i++ {
+			sub_sequence := sequence[i : i+window]
+			skew := gcskewCalc(sub_sequence)
+			new_gcskew.Values = append(new_gcskew.Values, skew)
+		}
+
+		gcskews = append(gcskews, new_gcskew)
+
+	}
+
+	return gcskews, nil
+
+}
